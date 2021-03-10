0087f44c50c30a24dab934b0516dd510689cf709
Kaylei Burke <kaylei@unabridgedsoftware.com>
2021-03-10 16:28:40 -0600

    Display questions to the user and store answers in a simple array
    Also includes user response validation.

--- main.rb
+++ main.rb
@@ -2,7 +2,34 @@
 require "pry-byebug"
 require "csv"
 
-questions = CSV.parse(File.read("questions.csv"), headers: true)
-ratings = CSV.parse(File.read("ratings.csv"), headers: true)
-muppets = CSV.parse(File.read("muppets.csv"), headers: true)
-ratings_values = CSV.parse(File.read("ratings_values.csv"), headers: true)
+questions = CSV.read("questions.csv", headers: true)
+ratings = CSV.read("ratings.csv", headers: true)
+muppets = CSV.read("muppets.csv", headers: true)
+ratings_values = CSV.read("ratings_values.csv", headers: true)
+
+def print_instructions
+  puts "On a scale of 1 to 5, please indicate how much you agree with each of the following statements."
+  puts "Strongly Disagree | Disagree | Neutral | Agree | Strongly Agree"
+  puts "       1          |     2    |    3    |   4   |       5       "
+end
+
+# loop through questions
+print_instructions
+answers = []
+
+questions.each do |row|
+  puts "* " + row["question"] + " *"
+  answer = gets.chomp.to_i
+
+  while !answer.between?(1, 5) do
+    puts "That is an invalid input. Please provide a response between 1 and 5."
+    answer = gets.chomp.to_i
+  end
+
+  answers.push(answer)
+end
+
+puts "The answers are: " + answers
+
+
+
