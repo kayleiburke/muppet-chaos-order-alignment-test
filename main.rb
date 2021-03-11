62ed0185c87325ff8d084a2df72f67b557135d15
Kaylei Burke <kaylei@unabridgedsoftware.com>
2021-03-10 18:18:46 -0600

    Perform calculations to find the muppet that fits the user's personality based on their responses to the questions.

--- main.rb
+++ main.rb
@@ -4,8 +4,8 @@ require "csv"
 
 questions = CSV.read("questions.csv", headers: true)
 ratings = CSV.read("ratings.csv", headers: true)
-muppets = CSV.read("muppets.csv", headers: true)
-ratings_values = CSV.read("ratings_values.csv", headers: true)
+muppets = CSV.read("muppets.csv", headers: true).map {|row| row.to_hash }
+ratings_values = CSV.read("ratings_values.csv", headers: true).map {|row| row.to_hash }
 
 def print_instructions
   puts "On a scale of 1 to 5, please indicate how much you agree with each of the following statements."
@@ -13,9 +13,37 @@ def print_instructions
   puts "       1          |     2    |    3    |   4   |       5       "
 end
 
-# loop through questions
+def calculate_result(responses, ratings_values, muppets)
+  responses.each do |response|
+    question = response[:question]
+    answer = response[:answer]
+
+    result = ratings_values.find {|row| row["question_id"] == question.to_s && row["rating_id"] == answer.to_s }
+
+    if result
+      muppets.map do |m|
+        if m["id"].to_s == result["muppet_id"].to_s
+          if m["count"]
+            m["count"] = m["count"] + 1
+          else
+            m["count"] = 1
+          end
+        end
+        m
+      end
+    end
+  end
+
+  muppets.max_by{|m| m["count"] }
+end
+
+# sample_responses = [{:question=>"1", :answer=>3}, {:question=>"2", :answer=>1}, {:question=>"3", :answer=>2}, {:question=>"4", :answer=>3}, {:question=>"5", :answer=>4}, {:question=>"6", :answer=>2}, {:question=>"7", :answer=>3}]
+# larger_sample_responses = [{:question=>"1", :answer=>1}, {:question=>"2", :answer=>2}, {:question=>"3", :answer=>3}, {:question=>"4", :answer=>4}, {:question=>"5", :answer=>5}, {:question=>"6", :answer=>1}, {:question=>"7", :answer=>3}, {:question=>"8", :answer=>4}, {:question=>"9", :answer=>1}, {:question=>"10", :answer=>3}, {:question=>"11", :answer=>4}, {:question=>"12", :answer=>5}, {:question=>"13", :answer=>1}, {:question=>"14", :answer=>2}, {:question=>"15", :answer=>3}, {:question=>"16", :answer=>4}, {:question=>"17", :answer=>3}, {:question=>"18", :answer=>2}, {:question=>"19", :answer=>3}, {:question=>"20", :answer=>4}, {:question=>"21", :answer=>2}, {:question=>"22", :answer=>4}, {:question=>"23", :answer=>5}, {:question=>"24", :answer=>3}, {:question=>"25", :answer=>2}, {:question=>"26", :answer=>3}, {:question=>"27", :answer=>4}, {:question=>"28", :answer=>1}, {:question=>"29", :answer=>2}, {:question=>"30", :answer=>3}]
+# caras_responses = [{:question=>"1", :answer=>5}, {:question=>"2", :answer=>2}, {:question=>"3", :answer=>4}, {:question=>"4", :answer=>2}, {:question=>"5", :answer=>5}, {:question=>"6", :answer=>1}, {:question=>"7", :answer=>5}, {:question=>"8", :answer=>5}, {:question=>"9", :answer=>4}, {:question=>"10", :answer=>4}, {:question=>"11", :answer=>3}, {:question=>"12", :answer=>4}, {:question=>"13", :answer=>1}, {:question=>"14", :answer=>3}, {:question=>"15", :answer=>5}, {:question=>"16", :answer=>4}, {:question=>"17", :answer=>2}, {:question=>"18", :answer=>4}, {:question=>"19", :answer=>2}, {:question=>"20", :answer=>4}, {:question=>"21", :answer=>1}, {:question=>"22", :answer=>2}, {:question=>"23", :answer=>2}, {:question=>"24", :answer=>3}, {:question=>"25", :answer=>2}, {:question=>"26", :answer=>4}, {:question=>"27", :answer=>4}, {:question=>"28", :answer=>2}, {:question=>"29", :answer=>5}, {:question=>"30", :answer=>5}]
+# calculate_result(caras_responses, ratings_values, muppets)
+
 print_instructions
-answers = []
+responses = []
 
 questions.each do |row|
   puts "* " + row["question"] + " *"
@@ -26,10 +54,14 @@ questions.each do |row|
     answer = gets.chomp.to_i
   end
 
-  answers.push(answer)
+  responses.push({ question: row[row.headers.first], answer: answer})
 end
 
-puts "The answers are: " + answers
+result = calculate_result(responses, ratings_values, muppets)
+
+puts "=====Your result is:====="
+puts result["label"]
+puts result["description"]
 
 
 
