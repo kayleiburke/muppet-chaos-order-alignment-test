84141d7a3baab151dd9c85720847e14279c7218d
Kaylei Burke <kaylei@unabridgedsoftware.com>
2021-03-24 13:32:13 -0500

    Create a macro called attr_int

--- models/choice.rb
+++ models/choice.rb
@@ -1,7 +1,7 @@
 class Choice
   extend ActiveRecordLite::ClassMethods
 
-  attr_accessor :id, :label, :question_id, :order
+  attr_accessor :label
 
   def initialize(id:, label:, question_id:, order:)
     @id = id
@@ -9,4 +9,18 @@ class Choice
     @question_id = question_id
     @order = order
   end
+
+  def self.attr_int(*args)
+    args.each do |method_name|
+      define_method(method_name) do
+        instance_variable_get("@#{method_name.to_s}").to_i
+      end
+
+      define_method("#{method_name}=") do |new_val|
+        instance_variable_set("@#{method_name.to_s}", new_val.to_i)
+      end
+    end
+  end
+
+  attr_int :id, :question_id, :order
 end
